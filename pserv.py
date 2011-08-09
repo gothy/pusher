ee1a8cdc9b11838d2c718b244d8e742d4f2a5304
Dmitry Utkin <dmitry.utkin@gmail.com>
2011-08-09 19:06:07 +0300

    constants like queue name, server address, etc. to config file

--- pserv.py
+++ pserv.py
@@ -1,4 +1,5 @@
 from os import path as op
+from optparse import OptionParser
 
 import json
 import tornado.web
@@ -7,30 +8,38 @@ import tornadio.router
 import tornadio.server
 
 from pika.adapters.tornado_connection import TornadoConnection
+import pika
 
 class NotiPikator(object):
     '''
     This is a singleton-wannabe class for connecting, listening and triggering events from RabbitMQ.
     It uses the <pika> library with adapter to internal Tornado ioloop for non-blocking operations on MQ.
     '''
-    listeners = {}
+    _listeners = {}
+    _host = 'localhost'
+    _port = 5672
+    
+    def __init__(self, *args, **kwargs):
+        self._host = kwargs.get('host', 'localhost')
+        self._port = kwargs.get('port', 5672)
     
     def add_listener(self, rkey, callback):
         'Add listener callback for a specific routing key'
         
-        self.listeners[rkey] = callback
-        self.channel.queue_bind(exchange='test', routing_key=str(rkey), queue='newq')
+        self._listeners[rkey] = callback
+        self.channel.queue_bind(exchange='mail', routing_key=str(rkey), queue='mailq')
 
     def remove_listener(self, rkey):
         'Remove callback for a routing key'
         
-        del self.listeners[rkey]
-        self.channel.queue_unbind(exchange='test', routing_key=str(rkey), queue='newq')
+        del self._listeners[rkey]
+        self.channel.queue_unbind(exchange='mail', routing_key=str(rkey), queue='mailq')
 
     def connect(self):
         'Establish RabbitMQ connection.'
         
-        self.connection = TornadoConnection(on_open_callback=self.on_connected)
+        self.connection = TornadoConnection(pika.ConnectionParameters(host=self._host, port=self._port), 
+                                            on_open_callback=self.on_connected)
 
     def on_connected(self, connection):
         'Callback for successfully established connecction'
@@ -43,8 +52,8 @@ class NotiPikator(object):
         
         print 'on_channel_open'
         self.channel = channel
-        self.channel.exchange_declare(exchange='test', type='direct')
-        self.channel.queue_declare(queue='newq',
+        self.channel.exchange_declare(exchange='mail', type='direct')
+        self.channel.queue_declare(queue='mailq',
                 callback=self.on_queue_declared,
                 arguments={'x-message-ttl':30000})
 
@@ -52,19 +61,18 @@ class NotiPikator(object):
         'Callback for successfull exchange hub and a queue on it declaration'
         
         print 'on_queue_declared'
-        self.channel.basic_consume(self.on_message, queue='newq', no_ack=True)
+        self.channel.basic_consume(self.on_message, queue='mailq', no_ack=True)
 
     def on_message(self, ch, method, properties, body):
         'Callback on incoming event for some binded routing_key'
         
         print " [x] Received %s %s" % (method.routing_key, body,)
-        self.listeners[method.routing_key](body)
+        self._listeners[method.routing_key](body)
 
 
 class IndexHandler(tornado.web.RequestHandler):
     """Regular HTTP handler to serve the chatroom page"""
-    def get(self):
-        self.render("index.html")
+    pass
 
 
 class PushConnection(tornadio.SocketConnection):
@@ -90,8 +98,6 @@ class PushConnection(tornadio.SocketConnection):
 
 #use the routes classmethod to build the correct resource
 PushRouter = tornadio.get_router(PushConnection)
-notipikator = NotiPikator()
-notipikator.connect()
 
 #config the Tornado application
 application = tornado.web.Application(
@@ -102,9 +108,25 @@ application = tornado.web.Application(
                          'xhr-polling'],
     #flash_policy_port = 843,
     #flash_policy_file = op.join(ROOT, 'flashpolicy.xml'),
-    socket_io_port = 8001
+    socket_io_port = 8001,
 )
 
 if __name__ == "__main__":
+    pathname = op.realpath(op.dirname(__file__))
+    parser = OptionParser()
+    parser.add_option("--mqhost", default='localhost', help="RabbitMQ host")
+    parser.add_option("--mqport", default=5672, help="RabbitMQ port")
+    parser.add_option("--bind_port", default=8001, help="socket.io listening port")
+    parser.add_option("--log", default=op.join(pathname, 'pserv.log'), help="log destination")
+    
+    (opts, args) = parser.parse_args()
+    
+    notipikator = NotiPikator(host=opts.mqhost, port=opts.mqport)
+    notipikator.connect()
+    
+    application.settings['socket_io_port'] = opts.bind_port
+    
+    print application.__dict__
+    
     tornadio.server.SocketServer(application)
     
